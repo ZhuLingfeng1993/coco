5516dbbad809245333b3df1f0aa0b30d8dfaadab
zhulingfeng <664675691@qq.com>
2020-03-20 22:52:55 +0800

    feat(test): Add demo.py and update view_result.py

--- PythonAPI/test/view_result.py
+++ PythonAPI/test/view_result.py
@@ -10,7 +10,8 @@ sys.path.insert(0, sys.path[0] + '/..')
 from pycocotools.coco import COCO
 import matplotlib.pyplot as plt
 import numpy as np
-import skimage.io as io
+# import skimage.io as io
+from PIL import Image
 
 # initialize COCO ground truth api
 annFile = 'test_set.json'
@@ -30,9 +31,10 @@ while True:
     imgId = imgIds[np.random.randint(len(imgIds))]
     print(imgId)
     img = cocoGt.loadImgs(imgId)[0]
-    I = io.imread(img['file_name'])
+    # I = io.imread(img['file_name'])
+    # print(I.shape)
+    I = Image.open(img['file_name'])
     print(img['file_name'])
-    print(I.shape)
 
     # visualize high confidence detections
     plt.imshow(I)
@@ -43,5 +45,5 @@ while True:
     for ann in anns:
         if ann['score'] > confidence_threshold:
             fanns.append(ann)
-    cocoDt.showAnns(fanns)
+    cocoDt.showAnns(fanns, draw_bbox=True)
     plt.show()
