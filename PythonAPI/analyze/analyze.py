32c025eddf30c885fcba460db18d76849f13ce9e
zhulingfeng <664675691@qq.com>
2020-04-18 20:19:05 +0800

    feat(analyze): Add bbox x center and y center and center-width analyze.

--- PythonAPI/analyze/analyze.py
+++ PythonAPI/analyze/analyze.py
@@ -69,6 +69,8 @@ areas_seq = []
 aspect_ratios_seq = []
 widths_seq = []
 heights_seq = []
+x_centers_seq = []
+y_centers_seq = []
 for catId in cat_num_dict:
     cat_names.append(coco.getCatName(catId))
     areas = np.array(cat_areas_dict[catId])
@@ -79,10 +81,18 @@ for catId in cat_num_dict:
     widths_seq.append(widths)
     heights = np.array(cat_bbox_dict[catId])[:, 3]
     heights_seq.append(heights)
+    x_centers = np.array(cat_bbox_dict[catId])[:, 0] + np.array(
+        cat_bbox_dict[catId])[:, 2] / 2
+    x_centers_seq.append(x_centers)
+    y_centers = np.array(cat_bbox_dict[catId])[:, 1] + np.array(
+        cat_bbox_dict[catId])[:, 3] / 2
+    y_centers_seq.append(y_centers)
 all_areas = np.concatenate(areas_seq)
 all_aspect_ratios = np.concatenate(aspect_ratios_seq)
 all_widths = np.concatenate(widths_seq)
 all_heights = np.concatenate(heights_seq)
+all_x_centers = np.concatenate(x_centers_seq)
+all_y_centers = np.concatenate(y_centers_seq)
 
 
 def my_plot(data, label, name='', range=None, update_kwargs=None):
@@ -100,6 +110,22 @@ def my_plot(data, label, name='', range=None, update_kwargs=None):
     plt.grid(which='both')
     plt.show()
     plt.savefig("{}.jpg".format(name))
+
+
+def my_plot2d(data1, data2, name1='', name2='', range=None, update_kwargs=None):
+    kwargs = {'density': False}
+    if update_kwargs is not None:
+        kwargs.update(update_kwargs)
+    # _, _, patches = plt.hist(data, bins=bins, range=range, **kwargs)
+    plt.hist2d(data1, data2, range=range, **kwargs)
+    plt.xlabel(name1)
+    plt.ylabel(name2)
+    # plt.legend(patches, label, loc='upper right')
+    plt.minorticks_on()
+    plt.grid(which='both')
+    plt.colorbar()
+    plt.show()
+    plt.savefig("{}.jpg".format(name1 + '_and_' + name2))
 
 
 # plot
@@ -121,33 +147,63 @@ def my_plot(data, label, name='', range=None, update_kwargs=None):
 # update_kwargs = {'density': True, 'cumulative': True}
 # my_plot(all_areas, 'all', name='area_all_cum', range=None,
 #         update_kwargs=update_kwargs)
+from matplotlib import colors
+
+# # We can increase the number of bins on each axis
+# axs[0].hist2d(x, y, bins=40)
+#
+# # As well as define normalization of the colors
+# h2 = axs[1].hist2d(x, y, bins=40, norm=colors.LogNorm())
+#
+# # We can also define custom numbers of bins for each axis
+# axs[2].hist2d(x, y, bins=(80, 10), norm=colors.LogNorm())
+# plt.colorbar()
+
+plt.clf()
+# update_kwargs = {'bins':(25,50)}
+update_kwargs = {}
+my_plot2d(all_x_centers, all_widths, name1='all_x_centers', name2='all_widths',
+          range=[[0, 500], [0, 200]], update_kwargs=update_kwargs)
+plt.clf()
+update_kwargs = {}
+my_plot2d(all_y_centers, all_widths, name1='all_y_centers', name2='all_widths',
+          range=None, update_kwargs=update_kwargs)
 
 plt.clf()
 update_kwargs = {}
-my_plot(all_widths, 'all', name='all_widths', range=None,
+my_plot(all_x_centers, 'all', name='all_x_centers', range=None,
+        update_kwargs=update_kwargs)
+plt.clf()
+update_kwargs = {}
+my_plot(all_y_centers, 'all', name='all_y_centers', range=None,
         update_kwargs=update_kwargs)
 
-plt.clf()
-my_plot(all_widths, 'all', name='all_widths_range', range=(0, 400),
-        update_kwargs=update_kwargs)
-
-plt.clf()
-update_kwargs = {'density': True, 'cumulative': True}
-my_plot(all_widths, 'all', name='width_all_cum', range=None,
-        update_kwargs=update_kwargs)
-
-plt.clf()
-my_plot(all_heights, 'all', name='all_heights', range=None,
-        update_kwargs=update_kwargs)
-
-plt.clf()
-my_plot(all_heights, 'all', name='all_heights_range', range=(0, 10000),
-        update_kwargs=update_kwargs)
-
-plt.clf()
-update_kwargs = {'density': True, 'cumulative': True}
-my_plot(all_heights, 'all', name='height_all_cum', range=None,
-        update_kwargs=update_kwargs)
+# plt.clf()
+# update_kwargs = {}
+# my_plot(all_widths, 'all', name='all_widths', range=None,
+#         update_kwargs=update_kwargs)
+#
+# plt.clf()
+# my_plot(all_widths, 'all', name='all_widths_range', range=(0, 400),
+#         update_kwargs=update_kwargs)
+#
+# plt.clf()
+# update_kwargs = {'density': True, 'cumulative': True}
+# my_plot(all_widths, 'all', name='width_all_cum', range=None,
+#         update_kwargs=update_kwargs)
+#
+# plt.clf()
+# my_plot(all_heights, 'all', name='all_heights', range=None,
+#         update_kwargs=update_kwargs)
+#
+# plt.clf()
+# my_plot(all_heights, 'all', name='all_heights_range', range=(0, 10000),
+#         update_kwargs=update_kwargs)
+#
+# plt.clf()
+# update_kwargs = {'density': True, 'cumulative': True}
+# my_plot(all_heights, 'all', name='height_all_cum', range=None,
+#         update_kwargs=update_kwargs)
 
 # plt.clf()
 # my_plot(all_areas, 'all', name='area_all_cum', bins=30, range=(0, 10000),
@@ -167,6 +223,3 @@ my_plot(all_heights, 'all', name='height_all_cum', range=None,
 # update_kwargs = {'density': True, 'cumulative': True}
 # my_plot(all_aspect_ratios, 'all', name='aspect_ratio_all_cum', bins=30,
 #         range=None, update_kwargs=update_kwargs)
-
-
-
